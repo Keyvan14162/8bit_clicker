bab9115179996459117758508b4014b0051d7553
ismail_kyvsn <you@example.com>
2022-06-09 01:11:09 +0300

    Boss names and dialogues entered

--- lib/data/boss_names.dart
+++ lib/data/boss_names.dart
@@ -2,61 +2,60 @@ class BossNames {
   static const List<String> BOSS_NAMES = [
     // for 0 index proplem
     "", // stage=1 => BOSS_NAMES[stage] == Boss1
-    "Boss1",
-    "Boss2",
-    "Boss3",
-    "Boss4",
-    "Boss5",
-    "Boss6",
-    "Boss7",
-    "Boss8",
-    "Boss9",
-    "Boss10",
-    "Boss11",
-    "Boss12",
-    "Boss13",
-    "Boss14",
-    "Boss15",
-    "Boss16",
-    "Boss17",
-    "Boss18",
-    "Boss19",
-    "Boss20",
-    "Boss21",
-    "Boss22",
-    "Boss23",
-    "Boss24",
-    "Boss25",
-    "Boss26",
+    "DJ SKULL", // 1
+    "COOL PIZZA", // 2
+    "FANTASMA", // 3
+    "RGB STAR", // 4
+    "GENGAR", // 5
+    "KNIGHT MAN", // 6
+    "MONOKUMA", // 7
+    "PENNYWISE", // 8
+    "HERO KNIGHT", // 9
+    "RGB SKULL", // 10
+    "GOTH", // 11
+    "SANS", // 12
+    "MAJORA", // 13
+    "MC SKULL", // 14
+    "MEWTWO", // 15
+    "MASTER CHIEF", // 16
+    "NYAN CAT", // 17
+    "ILLUMUNATI", // 18
+    "TEARS", // 19
+    "JOLTEON", // 20
+    "PIKACHU", // 21
+    "HADOKEN", // 22
+    "GROUDON", // 23
+    "METTATON", // 24
+    "CTHULHU", // 25
+    "DRAGO", // 26
   ];
   static const List<String> BOSS_DIALOGUES = [
-    // for 0 index proplem
-    "", // stage=1 => BOSS_NAMES[stage] == Boss1
-    "Boss1 dialogue",
-    "Boss2 dialogue",
-    "Boss3 dialogue",
-    "Boss4 dialogue",
-    "Boss5 dialogue",
-    "Boss6 dialogue",
-    "Boss7 dialogue",
-    "Boss8 dialogue",
-    "Boss9 dialogue",
-    "Boss10 dialogue",
-    "Boss11 dialogue",
-    "Boss12 dialogue",
-    "Boss13 dialogue",
-    "Boss14 dialogue",
-    "Boss15 dialogue",
-    "Boss16 dialogue",
-    "Boss17 dialogue",
-    "Boss18 dialogue",
-    "Boss19 dialogue",
-    "Boss20 dialogue",
-    "Boss21 dialogue",
-    "Boss22 dialogue",
-    "Boss23 dialogue",
-    "Boss24 dialogue",
-    "Boss25 dialogue",
-    "Boss26 dialogue",
+    "",
+    "Rest in music!", // 1
+    "You can't hang with me", // 2
+    "Booo!!", // 3
+    "Red Green and Blue", // 4
+    "Hop it back at you faggots", // 5
+    "What's your last wish ?", // 6
+    "Thrills, chills, kills!", // 7
+    "I will be your worst fear!", // 8
+    "You can't pass!", // 9
+    "It's okey to die!", // 10
+    "The Only Way Out Is To DIE!!!!", // 11
+    "You feel like you're going to have a bad time", // 12
+    "I didn't steal the mask!", // 13
+    "You can't kill a dead!", // 14
+    "Who am I and why am I here? I just appeared here. I haven't even been born to this world yet. Who am I?", // 15
+    "I'll Find Cortana's Solution, And I'll Bring It Back.", // 16
+    "Nyanyanyanyanyanyanya", // 17
+    "Don’t the Illuminati realize they’re just pawns in someone else’s game?", // 18
+    "My tears will drown the whole world!", // 19
+    "There is gonna be some power shortage!", // 20
+    "Pika Pika ?", // 21
+    "You will see my dark side! Just don't hide!", // 22
+    "I am the definition of SWAG!", // 23
+    "Who needs the sun when you've got the spotlight!!?", // 24
+    "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. In his house at R'lyeh dead Cthulhu waits dreaming.", // 25
+    "You fools! You have fallen prey to this trap! Don't you realize that you have just been tricked into reading this post in my ridiculously cool voice! -DRAGO", // 26
   ];
 }
